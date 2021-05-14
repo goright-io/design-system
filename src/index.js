bfb7f737f30c9bfafde5d9e59985606560257b2c
Irina Illustrova <illustrova@gmail.com>
2021-05-14 03:36:15 +0300

    uncommited files

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@
  * It's being updated automatically by add-component script
  * Don't edit it directly, your chages will be overwritten.
  */
-import "./global.css";
+import "./global.css"
+export { default as GlobalStyle } from "./global.js";
 export { colors, typography, spacing } from "./tokens";
+
+
+export { default as Button } from "./components/Button";
+export { default as Text } from "./components/Text";
+export { default as Typeset } from "./components/Typeset";
