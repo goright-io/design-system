804ff36d766256cca46b07149c00ecd85f3a3cb8
Irina Illustrova <illustrova@gmail.com>
2021-05-14 03:35:35 +0300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
+// const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("./src/tokens/dist/colors.json");
+const typography = require("./src/tokens/dist/typography.json");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   mode: "jit",
   purge: ["./src/**/*.{js,jsx,md,mdx}"],
   theme: {
-    extend: {},
+    // extend: {
+    colors: {
+      ...colors,
+    },
+    fontSize: { ...typography.fontSize },
+    fontWeight: { ...typography.fontWeight },
+    lineHeight: { ...typography.lineHeight },
+    letterSpacing: { ...typography.letterSpacing },
+    fontFamily: {
+      base: [typography.fontFamily.base, ...defaultTheme.fontFamily.sans],
+    },
   },
+  // },
 };
