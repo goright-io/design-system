93a045b0e9a05febc1e90824dea0ee61675611a0
Irina Illustrova <illustrova@gmail.com>
2021-05-17 22:55:11 +0300

    add Nav component

--- src/index.js
+++ src/index.js
@@ -8,4 +8,5 @@ import "./global.css"
 
 export { default as Button } from "./components/Button";
 export { default as Logo } from "./components/Logo";
+export { default as Nav } from "./components/Nav";
 export { default as Text } from "./components/Text";
