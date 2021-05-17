58b5a7e5ecd1cbfc9dfdef4ca8f21626219e4176
Irina Illustrova <illustrova@gmail.com>
2021-05-17 14:03:36 +0300

    add Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,39 @@
+/*
+ * Button component. It provides interfaces for all types
+ * of the buttons.
+ */
+
+import React from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+const variants = {
+  small:
+    "py-2 px-3  text-baseBolder font-baseBolder tracking-baseBolder leading-baseBolder rounded-sm",
+  big: "py-4 px-6 text-xlBolder font-xlBolder tracking-xlBolder leading-xlBolder rounded",
+};
+const Button = ({ href, label, children, variant, ...props }) => {
+  console.log("🚀 ~ file: index.js ~ line 15 ~ Button ~ href", href);
+  const Component = href ? "a" : "button";
+  console.log("🚀 ~ file: index.js ~ line 16 ~ Button ~ Component", Component);
+  return (
+    <Component
+      href={href}
+      className={classnames(
+        variant ? variants[variant] : variants.big,
+        "bg-primary-500 text-light-background-900 inline-flex items-center"
+      )}
+      {...props}
+    >
+      {label ? label : children}
+    </Component>
+  );
+};
+
+Button.propTypes = {
+  label: PropTypes.string,
+  children: PropTypes.node,
+  href: PropTypes.string,
+  variant: PropTypes.string,
+};
+
+export default Button;
