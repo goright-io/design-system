135233aa7ca74f7dff2af7244a19d5c6b6dd6cea
Irina Illustrova <illustrova@gmail.com>
2021-06-02 01:27:45 +0300

    pass classname as prop to Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,15 +2,21 @@ import PropTypes from "prop-types";
 import React from "react";
 import Text from "../Text";
 import CtaLink from "../CtaLink";
+import classnames from "classnames";
 
 /* Card
  *
  * TODO: add component description
  */
-const Card = ({ heading, link, children }) => {
-  const linkText = link.text;
+const Card = ({ heading, link, children, className }) => {
+  const linkText = link ? link.text : null;
   return (
-    <div className="flex flex-col p-6 border border-light-on-background-900 hover:bg-primary-50 text-light-on-background-900">
+    <div
+      className={classnames(
+        "flex flex-col h-full p-6 border border-light-on-background-900 hover:bg-primary-50 text-light-on-background-900",
+        className
+      )}
+    >
       {heading && (
         <Text as="h3" variant="2Xl" className="mb-6">
           {heading}
