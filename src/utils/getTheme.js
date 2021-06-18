1a0d31e55cb11c5a0d303fe7226ff65b05f504ee
Irina Illustrova <illustrova@gmail.com>
2021-06-19 00:58:52 +0300

    add testimonial carousel

--- src/utils/getTheme.js
+++ src/utils/getTheme.js
@@ -3,6 +3,15 @@ import tailwindConfig from "../../tailwind.config";
 
 export default resolveConfig(tailwindConfig).theme;
 
+export const getBreakpoints = () => {
+  const screens = resolveConfig(tailwindConfig).theme.screens;
+  let breakpoints = {};
+  for (let screen in screens) {
+    breakpoints[screen] = parseInt(screens[screen]);
+  }
+  return breakpoints;
+};
+
 export function getColors() {
   const generateColors = (colors, prefix) =>
     Object.keys(colors).reduce((acc, key) => {
